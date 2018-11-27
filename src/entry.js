1b59c345be8281e6b7e350786b9d652d37590e17
jonsilver99 <jonatanza1@gmail.com>
2018-11-27 10:19:08 +0200

    initial commit

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,7 @@
 import './styles.css'
-// vast-fetch module
+// modules
 import { vastFetch } from './modules/vast-fetch/vast-fetch'
-import { vastFetchTemplate } from './modules/vast-fetch/template'
-// vast-form module 
 import { vastForm } from './modules/vast-form/vast-form'
-import { vastFormTemplate } from './modules/vast-form/template'
 
 $(document).ready(function () {
 
@@ -13,15 +10,12 @@ $(document).ready(function () {
 
     toggleButtons.click(function () {
         let clicked = this
-
         // highlight clicked button
         toggleButtons.removeClass('selected');
         $(clicked).addClass('selected');
-
         // append and initialize selected module
         if (clicked.id == 'toggle-vast-fetch')
             vastFetch.initialize(parentContainer);
-
         else if (clicked.id == 'toggle-vast-form')
             vastForm.initialize(parentContainer);
     })
