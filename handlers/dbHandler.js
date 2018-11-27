ac2d88f57114c5a9dfe12a466dd6e7e57093ad67
jonsilver99 <jonatanza1@gmail.com>
2018-11-27 11:25:11 +0200

    debuggin mysql

--- handlers/dbHandler.js
+++ handlers/dbHandler.js
@@ -18,20 +18,40 @@ function sanitize(input) {
 
 function fetch(q) {
     return new Promise((resolve, reject) => {
-        dbConnection.query(q, (err, results) => {
+        dbConnection.connect((err) => {
+            if (err) console.log(err)
+            else console.log('connected')
+        })
+
+        let s = dbConnection.query(q, (err, results) => {
             if (err) reject(err)
             else if (results.length > 1) resolve(results);
             else resolve(results[0])
+        })
+
+        dbConnection.end((err) => {
+            if (err) console.log(err)
+            else console.log('connected')
         })
     })
 }
 
 function insert(q) {
+    dbConnection.connect((err) => {
+        if (err) console.log(err)
+        else console.log('connected')
+    })
+
     return new Promise((resolve, reject) => {
         dbConnection.query(q, (err, result) => {
             if (err) reject(err)
             else resolve(result)
         })
+    })
+
+    dbConnection.end((err) => {
+        if (err) console.log(err)
+        else console.log('connected')
     })
 }
 
